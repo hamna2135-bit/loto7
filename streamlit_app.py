b1808e4890a43a8c92d800b948876b1067f8060b
hamna2135-bit <hamna2135@gmail.com>
2026-07-03 02:32:23 +0000

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,19 @@ import streamlit as st
 st.title("🎱 ロト7番号ピックアップ")
 st.write("""どっかの攻略法を設計思想として、なんかありそうな番号をピックするよ
          クイックピック、ランダムピックではないからね""")
+#設計思想
+#1.引っ張り数字は「1つ」必ず入れる
+#2.連番は「1組」必ず入れる
+#3.末尾（下一桁）を「1組」合わせる
+#4.奇数・偶数のバランスは「3:4」か「4:3」
+#5.低中高のテーブルで均一になるように
+#6.6個ずつのテーブルでまんべんなく
+
+red = [n for n in range(1,7)]
+orange = [n+6 for n in red]
+yellow = [n+6 for n in orange]
+green = [n+6 for n in yellow]
+blue = [n+6 for n in green]
+purple = [n+6 for n in blue]
+purple.append(37)
+
