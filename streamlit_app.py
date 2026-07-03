95d1a4da7ca69aa891f24177308d5431823a0687
hamna2135-bit <hamna2135@gmail.com>
2026-07-03 02:26:22 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎱 ロト7番号ピックアップ")
+st.write("""どっかの攻略法を設計思想として、なんかありそうな番号をピックするよ
+         クイックピック、ランダムピックではないからね""")
